ac61dd4d214afce9a3fd9a275873802e33adaddf
Zanzong43 <alexkoszuta43@gmail.com>
2021-04-29 21:08:47 -0400

    Added basic added/deleting projects and bugs

--- bug.py
+++ bug.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from flask import Flask, g, render_template, request, redirect, session, url_for  # Import Flask to allow us to create our app
+from flask import Flask, g, render_template, request, redirect, session, url_for  
 from mysqlconnection import connectToMySQL
 
 
@@ -10,7 +10,7 @@ users = []
 
 
 
-app = Flask(__name__)    # Create a new instance of the Flask class called "app"
+app = Flask(__name__)    
 app.secret_key = 'secKey'
 
 @app.before_request
@@ -80,6 +80,62 @@ def register():
 
     return render_template('register.html')
 
+
+@app.route('/newProj', methods=['GET', 'POST'])
+def newProj():
+    if request.method == 'POST':
+        name = request.form['name']
+        scope = request.form['scope']
+        githubLink = request.form['githubLink']
+        lang = request.form['lang']
+        finished = 0
+        user_id = session['user_id']
+        mysql = connectToMySQL('bugTracker')
+        query='INSERT INTO projects (name, scope, lang, githubLink, finished, user_id) VALUES (%(name)s, %(scope)s, %(lang)s, %(githubLink)s, %(finished)s, %(user_id)s);'
+        data = {
+            "name": name,
+            "scope": scope,
+            "lang": lang,
+            "githubLink": githubLink,
+            "finished": finished,
+            "user_id": user_id,
+            }
+            
+        mysql.query_db(query, data)
+        return redirect(url_for('profile'))
+
+    return render_template('newProj.html')
+
+
+@app.route('/newBug', methods=['GET', 'POST'])
+def newBug():
+    if request.method == 'POST':
+        code = request.form['code']
+        description = request.form['description']
+        project_id = session['project_id']
+        user_id = session['user_id']
+
+        mysql = connectToMySQL('bugTracker')
+        query='INSERT INTO bugs (Code, Description, Solved, cause, project_id, user_id) VALUES (%(code)s, %(description)s, %(solved)s, %(cause)s, %(project_id)s, %(user_id)s);'
+        data = {
+            "code": code,
+            "description": description,
+            "solved": 0,
+            "cause": "unkown",
+            "project_id": project_id,
+            "user_id": user_id,
+            }
+            
+        mysql.query_db(query, data)
+        return redirect(url_for('project', project_id=session['project_id']))
+
+    return render_template('newBug.html')
+
+
+
+
+
+
 @app.route('/profile')
 def profile():
         projects = []
@@ -96,6 +152,7 @@ def profile():
 @app.route('/project/<project_id>')
 def project(project_id):
         session.pop('project', None)
+        session['project_id'] = project_id
         mysql = connectToMySQL('bugTracker')
         query='SELECT * FROM projects WHERE id=%(project_id)s;'
         data = {
@@ -107,11 +164,55 @@ def project(project_id):
         mysql = connectToMySQL('bugTracker')
         query='SELECT * FROM bugs WHERE project_id=%(project_id)s;'
         data = {
-            "project_id": project['id']
+            "project_id": project_id
             }   
         bugs = mysql.query_db(query, data)
         print(project, bugs)
         return render_template('project.html', project=project, bugs=bugs)
+
+
+
+@app.route('/deleteProject/<project_id>')
+def deleteProject(project_id):    
+    print("delete")   
+
+    mysql = connectToMySQL('bugTracker')
+    query='DELETE FROM projects WHERE id=%(project_id)s;'
+    data = {
+        "project_id": project_id
+        }     
+    mysql.query_db(query, data)      
+    print("done with db")
+
+    mysql = connectToMySQL('bugTracker')
+    query='DELETE FROM bugs WHERE project_id=%(project_id)s;'
+    data = {
+       "project_id": project_id
+       }   
+    mysql.query_db(query, data)
+
+    print("done with db")
+    return redirect(url_for('profile'))
+
+
+@app.route('/deleteBug/<bug_id>')
+def deleteBug(bug_id):    
+    print("delete")   
+
+    mysql = connectToMySQL('bugTracker')
+    query='DELETE FROM bugs WHERE id=%(bug_id)s;'
+    data = {
+        "bug_id": bug_id
+        }     
+    mysql.query_db(query, data)      
+    return redirect(url_for('project', project_id=session['project_id']))
+
+
+
+
+
+
+
 
 
 
